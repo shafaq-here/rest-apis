79b5ca097e60eb79dfb5b18a2a0aeea56d565229
shafaq-here <shafaqisrail@gmail.com>
2023-07-13 18:49:28 +0530

    Added Documentation for the .StrictSlash() function for better readability

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	/*The StrictSlash function in the mux package controls the behavior of the router when it comes to trailing slashes. When StrictSlash is set to true, the router will redirect requests that end with a trailing slash to the same path without the trailing slash. For example, a request to /articles/ will be redirected to /articles. When StrictSlash is set to false, the router will not redirect requests with trailing slashes.*/
 	router.HandleFunc("/", Index)
 	log.Fatal(http.ListenAndServe(":8080", router))
 	//log.Fatal(http.ListenAndServe(":8080", nil))
